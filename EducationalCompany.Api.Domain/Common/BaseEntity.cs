47849148a731634c6a36e79d9dfe717785e095d7
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-01-29 20:22:02 +0100

    feat(domain): add BaseEntity for tracking entity identity and timestamps

---
+++ EducationalCompany.Api.Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace EducationalCompany.Api.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        public DateTime? UpdatedAt { get; protected set; } 
+
+        protected BaseEntity()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+        public void UpdateModifiedDate()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
