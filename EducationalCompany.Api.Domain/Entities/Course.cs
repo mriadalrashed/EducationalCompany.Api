06d966352db21186fa0073b7c9fa9ef23a1f20f3
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-01-30 19:43:23 +0100

    feat(domain): add Course entity with properties, constructor, update method, and navigation to CourseOccasion

---
+++ EducationalCompany.Api.Domain/Entities/Course.cs
@@ -0,0 +1,37 @@
+﻿using EducationalCompany.Api.Domain.Common;
+using System.Collections.ObjectModel;
+
+
+namespace EducationalCompany.Api.Domain.Entities
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int DurationHours { get; private set; }
+        public decimal Price { get; private set; }
+        public ICollection<CourseOccasion> Occasions { get; private set; } = new List<CourseOccasion>();
+
+        protected Course()
+        {
+        }
+
+        public Course(string name, string description, int durationHours, decimal price)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(name)); ;
+            DurationHours = durationHours > 0 ? durationHours : throw new ArgumentException("duration must be positive");
+            Price = price >= 0 ? price : throw new ArgumentException("price can't be nagtive");
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void Update(string name, string description, int durationHours, decimal price)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(name)); ;
+            DurationHours = durationHours > 0 ? durationHours : throw new ArgumentException("duration must be positive");
+            Price = price >= 0 ? price : throw new ArgumentException("price can't be nagtive");
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
