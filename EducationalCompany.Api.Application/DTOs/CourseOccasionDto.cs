4b4cc18c4366701aef4b7bc0ecf2f0c425994fa8
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-01 17:55:15 +0100

    feat(application): add CourseOccasion DTOs for transfer, creation, and teacher assignment

---
+++ EducationalCompany.Api.Application/DTOs/CourseOccasionDto.cs
@@ -0,0 +1,38 @@
+﻿
+namespace EducationalCompany.Api.Application.DTOs
+{
+    public class CourseOccasionDto
+    {
+        public Guid CourseId { get; set; }
+        public Guid TeacherId { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxParticipants { get; set; }
+        public int CurrentParticipants { get; set; }
+        public bool IsFull => CurrentParticipants >= MaxParticipants;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public CourseDto Course { get;  set; }
+        public TeacherDto Teacher { get;  set; }
+    }
+
+    public class CreateCourseOccasionDto
+    {
+        public Guid CourseId { get; set; }
+        public Guid TeacherId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxParticipants { get; set; }
+    }
+    public class UpdateCourseOccasionDto
+    {
+        public int MaxParticipants { get; set; }
+    }
+
+    public class AssignTeacherDto
+    {
+        public Guid TeacherId { get; set; }
+    }
+
+}
