c7c168fab2413b4b642e36ef79d5d36dd7f5bbe7
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-01 17:55:24 +0100

    feat(application): add CourseRegistration DTOs for registration data and status updates

---
+++ EducationalCompany.Api.Application/DTOs/CourseRegistrationDto.cs
@@ -0,0 +1,26 @@
+﻿namespace EducationalCompany.Api.Application.DTOs
+{
+    public class CourseRegistrationDto
+    {
+        public Guid ParticipantId { get;  set; }
+        public Guid CourseOccasionId { get;  set; }
+        public DateTime RegistrationDate { get;  set; }
+        public string Status { get;  set; }
+        public DateTime? ConfirmedAt { get;  set; }
+        public DateTime? CancelledAt { get;  set; }
+        public DateTime CreatedAt { get;  set; }
+        public DateTime? UpdatedAt { get;  set; }
+        public ParticipantDto Participant { get;  set; }
+        public CourseOccasionDto CourseOccasion { get;  set; }
+    }
+
+    public class CreateCourseRegistrationDto
+    {
+        public Guid ParticipantId { get; set; }
+        public Guid CourseOccasionId { get; set; }
+    }
+    public class UpdateCourseRegistrationDto
+    {
+        public string Status { get; set; }
+    } 
+}
