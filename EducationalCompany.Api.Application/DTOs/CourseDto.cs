302e3dc26e0f1a667e2f6cd2b8103c0242e2eb06
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-01 17:54:24 +0100

    feat(application): add Course DTOs for data transfer and creation/update operations

---
+++ EducationalCompany.Api.Application/DTOs/CourseDto.cs
@@ -0,0 +1,30 @@
+﻿namespace EducationalCompany.Api.Application.DTOs
+{
+    public class CourseDto 
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DurationHours { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+    public class CreateCourseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DurationHours { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class UpdateCourseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DurationHours { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
