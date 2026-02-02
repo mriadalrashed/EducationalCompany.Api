b7b4a8af763c5332e09e7f5129b16fb47866cbc2
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 19:17:56 +0100

    feat(application): add ICourseRegistrationService interface for managing course registrations

---
+++ EducationalCompany.Api.Application/Interfaces/ICourseRegistrationService.cs
@@ -0,0 +1,19 @@
+﻿using EducationalCompany.Api.Application.DTOs;
+using EducationalCompany.Api.Domain.Entities;
+
+namespace EducationalCompany.Api.Application.Interfaces
+{
+    public interface ICourseRegistrationService
+    {
+        Task<IEnumerable<CourseRegistrationDto>> GetAllRegistrationsAsync();
+        Task<CourseRegistrationDto> GetRegistrationByIdAsync(Guid id);
+        Task<CourseRegistrationDto> CreateRegistrationAsync(CreateCourseRegistrationDto dto);
+        Task<CourseRegistrationDto> UpdateRegistrationAsync(UpdateCourseRegistrationDto dto);
+        Task<bool> DeleteRegistrationAsync(Guid id);
+        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByParticipantAsync(Guid participantId);
+        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByOccasionAsync(Guid courseOccasionId);
+        Task<CourseRegistrationDto> GetRegistrationsDetailsAsync(Guid id);
+        Task ConfirmRegistrationssAsync (Guid id);
+        Task CancelRegistrationAsync (Guid id);
+    }  
+}
