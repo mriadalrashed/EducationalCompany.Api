ea7f251a09cf47369957b97035b25a6a2a128328
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 18:26:10 +0100

    feat(application): add ServiceExtensions for registering application services with DI

---
+++ EducationalCompany.Api.Application/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EducationalCompany.Api.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using EducationalCompany.Api.Application.Interfaces;
+
+namespace EducationalCompany.Api.Application.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ITeacherService,TeacherService>();
+            services.AddScoped<IParticipantService, ParticipantService>();
+            services.AddScoped<ICourseRegistrationService, CourseRegistrationService>();
+            services.AddScoped<ICourseOccasionService, CourseOccasionService>();
+            return services;
+        }
+    }
+}
