54c20cb0ae241af8fd49fb93883ed9950c5fa515
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 19:17:38 +0100

    feat(application): add IParticipantService interface for participant management

---
+++ EducationalCompany.Api.Application/Interfaces/IParticipantService.cs
@@ -0,0 +1,17 @@
+﻿using EducationalCompany.Api.Application.DTOs;
+using EducationalCompany.Api.Domain.Entities;
+
+namespace EducationalCompany.Api.Application.Interfaces
+{
+    public interface IParticipantService
+    {
+        Task<IEnumerable<ParticipantDto>> GetAllIParticipantsAsync();
+        Task<ParticipantDto> GetParticipantByIdAsync(Guid id);
+        Task<ParticipantDto> CreateParticipantAsync(CreateParticipantDto createTeacherDto);
+        Task<ParticipantDto> UpdateParticipantAsync(UpdateParticipantDto updateTeacherDto);
+        Task<bool> DeleteParticipantAsync(Guid id);
+
+        Task<IEnumerable<ParticipantDto>> SearchParticipantsAsync(string searchTerm);
+        Task<ParticipantDto> GetParticipantWithRegistrationsAsync(Guid id);
+    }
+}
