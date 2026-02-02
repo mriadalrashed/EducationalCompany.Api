024e172e6a128f0e0ace2a9c7e3b9edb7feda6da
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 19:18:04 +0100

    feat(application): add ICourseOccasionService interface for course occasion operations

---
+++ EducationalCompany.Api.Application/Interfaces/ICourseOccasionService.cs
@@ -0,0 +1,19 @@
+﻿using EducationalCompany.Api.Application.DTOs;
+using EducationalCompany.Api.Domain.Entities;
+
+namespace EducationalCompany.Api.Application.Interfaces
+{
+    public interface ICourseOccasionService
+    {
+        Task<IEnumerable<CourseOccasionDto>> GetAllOccasionsAsync();
+        Task<CourseOccasionDto> GetOccasionByIdAsync(Guid id);
+        Task<CourseOccasionDto> CreateOccasionAsync(CreateCourseOccasionDto dto);
+        Task<CourseOccasionDto> UpdateOccasionAsync(UpdateCourseOccasionDto dto);
+        Task<bool> DeleteOccasionAsync(Guid id);
+        Task<IEnumerable<CourseOccasionDto>> GetOccasionsByIdAsync(Guid courseId);
+        Task AssignTeacherAsync (Guid occasionId, AssignTeacherDto dto);
+        Task <CourseOccasionDto> GetOccasionWithRegstrationsAsync (Guid id);
+        Task <bool> IsOccasionFullAsync (Guid id);
+        Task <CourseOccasionDto> MaptoDto (CourseOccasion occasion);
+    }
+}
