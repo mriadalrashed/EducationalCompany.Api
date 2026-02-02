d8b2487f428f02ba07a263acbd85c4f111d0c9f4
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 19:17:46 +0100

    feat(application): add ICourseService interface for course management operations

---
+++ EducationalCompany.Api.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,15 @@
+﻿using EducationalCompany.Api.Application.DTOs;
+using EducationalCompany.Api.Domain.Entities;
+
+namespace EducationalCompany.Api.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseDto>> GetAllCoursesAsync();
+        Task<CourseDto> GetCourseByIdAsync(Guid id);
+        Task<CourseDto> CreateCourseAsync(CreateCourseDto dto);
+        Task<CourseDto> UpdateCourseAsync(UpdateCourseDto dto);
+        Task<bool> DeleteCourseAsync(Guid id);
+        Task<IEnumerable<CourseDto>> SearchCoursesAsync(string searchTerm);
+    }
+}
