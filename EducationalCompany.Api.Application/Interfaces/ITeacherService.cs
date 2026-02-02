9681dc3a55e61ba51fcea59b7a157cabdce84ffb
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-02 19:17:28 +0100

    feat(application): add ITeacherService interface for teacher management operations

---
+++ EducationalCompany.Api.Application/Interfaces/ITeacherService.cs
@@ -0,0 +1,17 @@
+﻿using EducationalCompany.Api.Application.DTOs;
+using EducationalCompany.Api.Domain.Entities;
+
+namespace EducationalCompany.Api.Application.Interfaces
+{
+    public interface ITeacherService
+    {
+        Task<IEnumerable<TeacherDto>> GetAllTeachersAsync();
+        Task<TeacherDto> GetTeacherByIdAsync(Guid id);
+        Task<TeacherDto> CreateTeacherAsync(CreateTeacherDto createTeacherDto);
+        Task<TeacherDto> UpdateTeacherAsync(UpdateTeacherDto updateTeacherDto);
+        Task<bool> DeleteTeacherAsync(Guid id);
+
+        Task<IEnumerable<TeacherDto>> SearchTeachersAsync(string searchTerm);
+        Task<TeacherDto> GetTeacherWithOccasionsAsync(Guid id);
+    }
+}
