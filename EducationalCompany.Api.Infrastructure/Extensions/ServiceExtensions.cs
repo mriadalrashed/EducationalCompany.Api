064a2a25da731327de4ff4ddea127117fc32eeb4
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-20 19:38:05 +0100

    feat(infrastructure): add infrastructure service registrations
    - Configured ApplicationDbContext to use PostgreSQL with connection string from configuration
    - Set MigrationsAssembly to keep EF Core migrations in Infrastructure project
    - Registered UnitOfWork as a scoped service
    - Ensures Dependency Injection is properly wired for database access and repository operations

---
+++ EducationalCompany.Api.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using EducationalCompany.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EducationalCompany.Api.Infrastructure.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructueServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"),b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
