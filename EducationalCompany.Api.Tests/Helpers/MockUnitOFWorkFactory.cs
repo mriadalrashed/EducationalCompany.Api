dafbb09d831b6113ffcfebd8fb4c5050ce461baa
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-21 22:28:56 +0100

    test: add MockUnitOfWorkFactory to centralize repository mocking with Moq
    - Created helper factory to mock IUnitOfWork
    - Configured mock repositories for:
      - Courses
      - Participants
      - CourseRegistrations
    - Linked mocked repositories to UnitOfWork properties
    - Simplifies service unit test setup and reduces boilerplate code

---
+++ EducationalCompany.Api.Tests/Helpers/MockUnitOFWorkFactory.cs
@@ -0,0 +1,23 @@
+﻿using EducationalCompany.Api.Domain.Entities;
+using EducationalCompany.Api.Infrastructure;
+using Moq;
+
+namespace EducationalCompany.Api.Tests.Helpers
+{
+    public static class MockUnitOFWorkFactory
+    {
+        public static (Mock<IUnitOFWork>,Mock<ICourseRepository>) Create()
+        {
+            var courseRepoMock = new Mock<ICourseRepository>();
+            var uowMock = new Mock<IUnitOFWork>();
+            var mockParticipantRepo = new Mock<IParticipantRepository>();
+            var mockRegistrationRepo = new Mock<ICourseRegistrationRepository>();
+
+            uowMock.Setup(u => u.Participants).Returns(mockParticipantRepo.Object);
+            uowMock.Setup(u => u.CourseRegistrations).Returns(mockRegistrationRepo.Object);
+            uowMock.Setup(u => u.Courses).Returns(courseRepoMock.Object);
+            return (uowMock, courseRepoMock);
+           
+        }
+    }
+}
