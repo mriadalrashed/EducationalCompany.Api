73a1ad3e02fee513004df590bf3a0c86736c28ec
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 18:54:15 +0100

    docs(application): add comments to ICourseRegistrationService

--- EducationalCompany.Api.Application/Interfaces/ICourseRegistrationService.cs
+++ EducationalCompany.Api.Application/Interfaces/ICourseRegistrationService.cs
@@ -3,17 +3,18 @@ using EducationalCompany.Api.Domain.Entities;
 
 namespace EducationalCompany.Api.Application.Interfaces
 {
+    // Interface that defines operations for course registrations
     public interface ICourseRegistrationService
     {
-        Task<IEnumerable<CourseRegistrationDto>> GetAllRegistrationsAsync();
-        Task<CourseRegistrationDto> GetRegistrationByIdAsync(Guid id);
-        Task<CourseRegistrationDto> CreateRegistrationAsync(CreateCourseRegistrationDto dto);
-        Task<CourseRegistrationDto> UpdateRegistrationAsync(UpdateCourseRegistrationDto dto);
-        Task<bool> DeleteRegistrationAsync(Guid id);
-        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByParticipantAsync(Guid participantId);
-        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByOccasionAsync(Guid courseOccasionId);
-        Task<CourseRegistrationDto> GetRegistrationDetailsAsync(Guid id);
-        Task ConfirmRegistrationsAsync (Guid id);
-        Task CancelRegistrationAsync (Guid id);
+        Task<IEnumerable<CourseRegistrationDto>> GetAllRegistrationsAsync(); // Get all registrations
+        Task<CourseRegistrationDto> GetRegistrationByIdAsync(Guid id); // Get registration by ID
+        Task<CourseRegistrationDto> CreateRegistrationAsync(CreateCourseRegistrationDto dto); // Create new registration
+        Task<CourseRegistrationDto> UpdateRegistrationAsync(UpdateCourseRegistrationDto dto); // Update registration
+        Task<bool> DeleteRegistrationAsync(Guid id);  // Delete registration
+        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByParticipantAsync(Guid participantId);  // Get registrations by participant
+        Task<IEnumerable<CourseRegistrationDto>> GetRegistrationsByOccasionAsync(Guid courseOccasionId);// Get registrations by course occasion
+        Task<CourseRegistrationDto> GetRegistrationDetailsAsync(Guid id); // Get registration with details
+        Task ConfirmRegistrationsAsync (Guid id);// Confirm registration
+        Task CancelRegistrationAsync (Guid id);// Cancel registration
     }  
 }
