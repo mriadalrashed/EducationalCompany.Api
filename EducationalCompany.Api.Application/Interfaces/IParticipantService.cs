018f3fae62f0a06a75b7e2bf3d99128b92650614
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 18:56:44 +0100

    docs(application): add comments to IParticipantService

--- EducationalCompany.Api.Application/Interfaces/IParticipantService.cs
+++ EducationalCompany.Api.Application/Interfaces/IParticipantService.cs
@@ -3,15 +3,16 @@ using EducationalCompany.Api.Domain.Entities;
 
 namespace EducationalCompany.Api.Application.Interfaces
 {
+    // Interface that defines operations for participant management
     public interface IParticipantService
     {
-        Task<IEnumerable<ParticipantDto>> GetAllIParticipantsAsync();
-        Task<ParticipantDto> GetParticipantByIdAsync(Guid id);
-        Task<ParticipantDto> CreateParticipantAsync(CreateParticipantDto createTeacherDto);
-        Task<ParticipantDto> UpdateParticipantAsync(UpdateParticipantDto updateTeacherDto);
-        Task<bool> DeleteParticipantAsync(Guid id);
+        Task<IEnumerable<ParticipantDto>> GetAllIParticipantsAsync(); // Get all participants
+        Task<ParticipantDto> GetParticipantByIdAsync(Guid id);  // Get participant by ID
+        Task<ParticipantDto> CreateParticipantAsync(CreateParticipantDto createTeacherDto); // Create new participant
+        Task<ParticipantDto> UpdateParticipantAsync(UpdateParticipantDto updateTeacherDto); // Update participant
+        Task<bool> DeleteParticipantAsync(Guid id);// Delete participant
 
-        Task<IEnumerable<ParticipantDto>> SearchParticipantsAsync(string searchTerm);
-        Task<ParticipantDto> GetParticipantWithRegistrationsAsync(Guid id);
+        Task<IEnumerable<ParticipantDto>> SearchParticipantsAsync(string searchTerm); // Search participants
+        Task<ParticipantDto> GetParticipantWithRegistrationsAsync(Guid id); // Get participant with registrations
     }
 }
