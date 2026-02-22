e45e5c2bcba4560aa61573e4a35612a4a2770243
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 15:03:39 +0100

    docs(domain): document Teacher entity and relationships

--- EducationalCompany.Api.Domain/Entities/Teacher.cs
+++ EducationalCompany.Api.Domain/Entities/Teacher.cs
@@ -2,6 +2,7 @@
 
 namespace EducationalCompany.Api.Domain.Entities
 {
+    // Represents a teacher who can be assigned to course occasions
     public class Teacher : BaseEntity
     {
         public string FirstName { get; private set; }
@@ -9,11 +10,16 @@ namespace EducationalCompany.Api.Domain.Entities
         public string Email { get; private set; }
         public string Phone { get; private set; }
         public string Specialization { get; private set; }
+
+        // List of course occasions taught by this teacher
         public ICollection<CourseOccasion> CourseOccasion { get; private set; } = new List<CourseOccasion>();
 
+        // List of course occasions taught by this teacher
         protected Teacher()
         {
         }
+
+        // Creates a new teacher with basic validation
         public Teacher(string firstName, string lastName, string email, string phoneNumber)
         {
             FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
@@ -23,6 +29,8 @@ namespace EducationalCompany.Api.Domain.Entities
             Specialization = Specialization ?? throw new ArgumentNullException(nameof(Specialization));
             CreatedAt = DateTime.UtcNow;
         }
+
+        // Updates teacher information
         public void Update(string firstName, string lastName, string email, string phoneNumber)
         {
             FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
