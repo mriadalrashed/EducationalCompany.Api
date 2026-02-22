313f5252997d36c7e7929fc5a0878845a250a312
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 18:39:40 +0100

    fix(application): fix DurationHours type

--- EducationalCompany.Api.Application/DTOs/CourseDto.cs
+++ EducationalCompany.Api.Application/DTOs/CourseDto.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime DurationHours { get; set; }
+        public int DurationHours { get; set; }
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
@@ -14,7 +14,7 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime DurationHours { get; set; }
+        public int DurationHours { get; set; }
         public decimal Price { get; set; }
     }
 
@@ -23,7 +23,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime DurationHours { get; set; }
+        public int DurationHours { get; set; }
         public decimal Price { get; set; }
     }
 }
