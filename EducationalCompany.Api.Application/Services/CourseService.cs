307ed13bc4f5c284d59a03344e8f5c8a82ecc55d
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 19:00:47 +0100

    docs(application): add documentation comments to CourseService

--- EducationalCompany.Api.Application/Services/CourseService.cs
+++ EducationalCompany.Api.Application/Services/CourseService.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace EducationalCompany.Api.Application.Services
 {
 
-
+    // Service responsible for course business logic
     public class CourseService : ICourseService
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -16,11 +16,15 @@ namespace EducationalCompany.Api.Application.Services
         {
             _unitOfWork = unitOfWork;
         }
+
+        // Get all courses
         public async Task<IEnumerable<CourseDto>> GetAllCoursesAsync()
         {
             var courses = await _unitOfWork.Courses.GetAllAsync();
             return courses.Select(MapToDto);
         }
+
+        // Get course by ID
         public async Task<CourseDto> GetCourseByIdAsync(Guid id)
         {
            var course = await _unitOfWork.Courses.GetByIdAsync(id);
@@ -30,6 +34,8 @@ namespace EducationalCompany.Api.Application.Services
 
            return MapToDto(course);
         }
+
+        // Create new course
         public async Task<CourseDto> CreateCourseAsync(CreateCourseDto dto)
         {
             if (await _unitOfWork.Courses.CourseNameExistsAsync(dto.Name))
@@ -47,6 +53,8 @@ namespace EducationalCompany.Api.Application.Services
             return MapToDto(course);
 
         }
+
+        // Update existing course
         public async Task UpdateCourseAsync(Guid id, UpdateCourseDto dto)
         {
            var course = await _unitOfWork.Courses.GetByIdAsync(id);
@@ -61,6 +69,8 @@ namespace EducationalCompany.Api.Application.Services
 
             await _unitOfWork.Courses.UpdateAsync(course);
         }
+
+        // Delete course
         public async Task DeleteCourseAsync(Guid id)
         {
             var course = await _unitOfWork.Courses.GetByIdAsync(id);
@@ -69,12 +79,15 @@ namespace EducationalCompany.Api.Application.Services
             await _unitOfWork.Courses.DeleteAsync(course);
         }
 
+
+        // Search courses
         public async Task<IEnumerable<CourseDto>> SearchCoursesAsync(string searchTerm)
         {
             var courses = await _unitOfWork.Courses.SearchAsync(searchTerm);
             return courses.Select(MapToDto);
         }
 
+        // Map entity to DTO
         private CourseDto MapToDto(Course course)
         {
             return new CourseDto
