3f8659325931d45b246f7e6060815a3d087e1ae1
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 18:43:30 +0100

    docs(application): add simple comments to Course DTOs

--- EducationalCompany.Api.Application/DTOs/CourseDto.cs
+++ EducationalCompany.Api.Application/DTOs/CourseDto.cs
@@ -1,5 +1,6 @@
 ﻿namespace EducationalCompany.Api.Application.DTOs
 {
+    // DTO used to return course data to the client
     public class CourseDto 
     {
         public Guid Id { get; set; }
@@ -10,6 +11,8 @@
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
+
+    // DTO used when creating a new course
     public class CreateCourseDto
     {
         public string Name { get; set; }
@@ -18,6 +21,8 @@
         public decimal Price { get; set; }
     }
 
+
+    // DTO used when updating an existing course
     public class UpdateCourseDto
     {
         public Guid Id { get; set; }
