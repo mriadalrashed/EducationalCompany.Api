de7c89dbc705ea2b66fbc0f84ab68881d1e15edd
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-22 18:44:58 +0100

    docs(application): add comments to CourseOccasion DTOs

--- EducationalCompany.Api.Application/DTOs/CourseOccasionDto.cs
+++ EducationalCompany.Api.Application/DTOs/CourseOccasionDto.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace EducationalCompany.Api.Application.DTOs
 {
+    // DTO used to return course occasion data
     public class CourseOccasionDto
     {
         public Guid CourseId { get; set; }
@@ -10,6 +11,8 @@ namespace EducationalCompany.Api.Application.DTOs
         public DateTime EndDate { get; set; }
         public int MaxParticipants { get; set; }
         public int CurrentParticipants { get; set; }
+
+        // Indicates if the occasion is full
         public bool IsFull => CurrentParticipants >= MaxParticipants;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
@@ -17,6 +20,7 @@ namespace EducationalCompany.Api.Application.DTOs
         public TeacherDto Teacher { get;  set; }
     }
 
+    // DTO used when creating a course occasion
     public class CreateCourseOccasionDto
     {
         public Guid CourseId { get; set; }
@@ -25,14 +29,17 @@ namespace EducationalCompany.Api.Application.DTOs
         public DateTime EndDate { get; set; }
         public int MaxParticipants { get; set; }
     }
+
+    // DTO used when updating course occasion
     public class UpdateCourseOccasionDto
     {
         public int MaxParticipants { get; set; }
     }
 
+    // DTO used to assign a teacher to a course occasion
     public class AssignTeacherDto
     {
-        public Guid TeacherId { get; set; }
+        public Guid TeacherId { get; set; } // Teacher ID
     }
 
 }
