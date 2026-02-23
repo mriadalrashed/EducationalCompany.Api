347cd910b7506ac4664da61ca7bd577c65edccde
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-23 18:48:58 +0100

    refactor(application): standardize service update and delete method signatures

--- EducationalCompany.Api.Application/Interfaces/IParticipantService.cs
+++ EducationalCompany.Api.Application/Interfaces/IParticipantService.cs
@@ -6,11 +6,11 @@ namespace EducationalCompany.Api.Application.Interfaces
     // Interface that defines operations for participant management
     public interface IParticipantService
     {
-        Task<IEnumerable<ParticipantDto>> GetAllIParticipantsAsync(); // Get all participants
+        Task<IEnumerable<ParticipantDto>> GetAllParticipantsAsync(); // Get all participants
         Task<ParticipantDto> GetParticipantByIdAsync(Guid id);  // Get participant by ID
         Task<ParticipantDto> CreateParticipantAsync(CreateParticipantDto createTeacherDto); // Create new participant
-        Task<ParticipantDto> UpdateParticipantAsync(UpdateParticipantDto updateTeacherDto); // Update participant
-        Task<bool> DeleteParticipantAsync(Guid id);// Delete participant
+        Task UpdateParticipantAsync(Guid id, UpdateParticipantDto updateTeacherDto); // Update participant
+        Task DeleteParticipantAsync(Guid id);// Delete participant
 
         Task<IEnumerable<ParticipantDto>> SearchParticipantsAsync(string searchTerm); // Search participants
         Task<ParticipantDto> GetParticipantWithRegistrationsAsync(Guid id); // Get participant with registrations
