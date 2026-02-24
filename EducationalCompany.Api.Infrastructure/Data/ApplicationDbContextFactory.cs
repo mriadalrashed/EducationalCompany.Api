38cf2135998146e630dcfb0ed2a1fd995e9e941a
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-24 16:01:19 +0100

    chore(db): comment out ApplicationDbContextFactory implementation
    Temporarily disable ApplicationDbContextFactory by wrapping entire file in block comment.

--- EducationalCompany.Api.Infrastructure/Data/ApplicationDbContextFactory.cs
+++ EducationalCompany.Api.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -1,27 +1,28 @@
-﻿using EducationalCompany.Api.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
+﻿/*    using EducationalCompany.Api.Infrastructure.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
 
-namespace EducationalCompany.Infrastructure.Data
-{
-    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    namespace EducationalCompany.Infrastructure.Data
     {
-        public ApplicationDbContext CreateDbContext(string[] args)
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
         {
-            // Build configuration to read appsettings.json
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory()) // make sure this points to the startup project folder
-                .AddJsonFile("appsettings.json")
-                .Build();
+            public ApplicationDbContext CreateDbContext(string[] args)
+            {
+                // Build configuration to read appsettings.json
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory()) // make sure this points to the startup project folder
+                    .AddJsonFile("appsettings.json")
+                    .Build();
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            optionsBuilder.UseNpgsql(connectionString);
+                //var connectionString = configuration.GetConnectionString("DefaultConnection");
+                //optionsBuilder.UseNpgsql(connectionString);
 
-            return new ApplicationDbContext(optionsBuilder.Options);
+
+                return new ApplicationDbContext(optionsBuilder.Options);
+            }
         }
-    }
-}
+    }*/
