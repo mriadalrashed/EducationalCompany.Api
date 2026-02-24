ae446ac038157a71f2ad17aeea0f8062f697ddf0
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-02-24 15:54:49 +0100

    refactor(dto): remove Id property from UpdateTeacherDto

--- EducationalCompany.Api.Application/DTOs/TeacherDto.cs
+++ EducationalCompany.Api.Application/DTOs/TeacherDto.cs
@@ -26,7 +26,7 @@
     // DTO used when updating teacher data
     public class UpdateTeacherDto
     {
-        public Guid Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
